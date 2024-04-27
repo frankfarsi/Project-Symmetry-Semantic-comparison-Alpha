f716e9601400771a1662b5ae0f5d2283a56cb6ae
elfarsif <f.n.elfarsi@gmail.com>
2024-04-27 18:50:29 -0400

    cleaner code
    -added a function to initialize request into model

--- api/flaskr/controllers/translateController.py
+++ api/flaskr/controllers/translateController.py
@@ -8,8 +8,9 @@ from flaskr.services import translateService
 translate_route = Blueprint("translate_route", __name__, url_prefix="/translate")
 
 
-@translate_route.route("/sourceAndTarget", methods=["POST"])
-def get_target_article():
+# TODO:make a method to put the initialize request object
+@translate_route.route("/targetArticleLanguages", methods=["POST"])
+def get_list_of_target_article_languages():
     translateArticleRequestObject = RequestModel(
         request.get_json().get("sourceArticleUrl"),
         request.get_json().get("targetLanguage"),
@@ -37,8 +38,14 @@ def get_source_article():
     return make_response({"message": "Settings are valid"}, 200)
 
 
+def initialize_request_object(requestObj):
+    initialized_request_object = RequestModel(
+        requestObj.get_json().get("sourceArticleUrl"),
+        requestObj.get_json().get("targetLanguage"),
+        requestObj.get_json().get("translationTool"),
+        requestObj.get_json().get("deepLApiKey"),
+    )
+    return initialized_request_object
+
+
 # TODO: see what flask has as a middleware built in
-def middlewareValidateSourceUrl(request: RequestModel) -> None:
-    if not request:
-        raise ValueError("Invalid request")
-    # regex that its
